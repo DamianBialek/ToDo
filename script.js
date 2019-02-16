6f516ebf8646b476fb390003339a763d2a278f78
Damian <damianbiaek1997@hotmail.com>
2019-02-16 15:54:30 +0100

    Add the option to delete the task, mark as done and corrections in operation and appearance

--- script.js
+++ script.js
@@ -1,45 +1,121 @@
 const renderTasks = function() {
-    var box = document.querySelector(".todos")
-    box.innerHTML = ''
+    const box = document.querySelector(".todos");
+    box.innerHTML = '';
 
-    if(tasks.length > 0)
-        tasks.forEach(function(el){
-            var newTaskDiv = document.createElement("div")
-            newTaskDiv.innerHTML = el.name
-            newTaskDiv.classList.add("task")
+    if(tasks.length > 0) {
+        document.querySelector("section.no-task").classList.add("hide");
 
-            var span = document.createElement("span")
-            span.classList.add("task-date")
-            span.innerHTML = el.date
+        tasks.forEach(function (el, idx) {
+            const taskBox = document.createElement("div");
+            taskBox.classList.add("task");
 
-            newTaskDiv.append(span)
+            if(el.done === true)
+                taskBox.classList.add("done");
 
-            box.append(newTaskDiv)
+
+            const newTaskDiv = document.createElement("div");
+            newTaskDiv.innerHTML = el.name;
+
+            const span = document.createElement("span");
+            span.classList.add("task-date");
+            span.innerHTML = el.date;
+
+            const taskDivBtns = document.createElement("div");
+            taskDivBtns.classList.add("task-btns")
+            taskDivBtns.setAttribute("data-task-id", idx);
+
+            const button = document.createElement("button");
+            button.classList.add("btn", "remove-btn");
+            button.setAttribute("title", "Usuń zadanie");
+
+            const icon = document.createElement("i");
+            icon.classList.add("icon", "minus-icon");
+
+
+            if(el.done === false) {
+                const tickButton = document.createElement("button");
+                tickButton.classList.add("btn", "tick-btn");
+                tickButton.setAttribute("title", "Oznacz jako ukończone");
+
+                const tickIcon = document.createElement("i");
+                tickIcon.classList.add("icon", "tick-icon");
+
+                tickButton.append(tickIcon);
+                taskDivBtns.append(tickButton);
+            }
+
+            button.append(icon);
+            newTaskDiv.append(span);
+            taskDivBtns.append(button);
+            taskBox.append(newTaskDiv);
+            taskBox.append(taskDivBtns);
+            box.append(taskBox);
         })
-}
+    }
+    else {
+        document.querySelector("section.no-task").classList.remove("hide")
+    }
+};
 
 const getDate = function() {
-    var now = new Date()
-    var dd = now.getDay()
-    var mm = now.getMonth() + 1
+    const now = new Date();
+    let dd = now.getDay();
+    let mm = now.getMonth() + 1;
 
-    dd = (dd < 10 ? "0"+dd : dd)
-    mm = (mm < 10 ? "0"+mm : mm)
+    dd = (dd < 10 ? "0"+dd : dd);
+    mm = (mm < 10 ? "0"+mm : mm);
 
     return now.getFullYear()+"-"+(mm)+"-"+dd
-}
+};
 
-var tasks = JSON.parse(localStorage.getItem("todoTasks")) || []
+const saveTasks = function() {
+    localStorage.setItem("todoTasks", JSON.stringify(tasks));
+};
 
-renderTasks()
+const getClickedTaskId = function(e) {
+    let clickedEl = e.target;
+
+    if(e.target.tagName === 'I')
+        clickedEl = e.target.parentElement;
+
+    return clickedEl.parentElement.getAttribute('data-task-id');
+};
+
+const removeTask = function(e) {
+    tasks.splice(getClickedTaskId(e), 1);
+    saveTasks();
+    renderTasks();
+};
+
+const markTaskAsDone = function(e) {
+    const task = tasks[getClickedTaskId(e)];
+    task.done = true;
+
+    tasks.splice(getClickedTaskId(e), 1);
+    tasks.push(task);
+
+    saveTasks();
+    renderTasks();
+};
+
+const tasks = JSON.parse(localStorage.getItem("todoTasks")) || [];
 
 document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("newTaskBtn").addEventListener("click", function() {
-        var taskValue = document.querySelector("[name='newTask']").value
-        document.querySelector("[name='newTask']").value = ''
-        tasks.push({name: taskValue, date: getDate()})
+    renderTasks();
 
-        localStorage.setItem("todoTasks", JSON.stringify(tasks))
+    document.querySelector("#newTaskBtn").addEventListener("click", function() {
+        const taskValue = document.querySelector("[name='newTask']").value;
+        document.querySelector("[name='newTask']").value = '';
+        tasks.unshift({name: taskValue, date: getDate(), done: false});
+
+        saveTasks();
         renderTasks()
-    })
-})
+    });
+
+    document.addEventListener("click", (e) => {
+        if (e.target && (e.target.matches(".remove-btn") || e.target.matches(".remove-btn i")))
+            removeTask(e);
+        else if (e.target && (e.target.matches(".tick-btn") || e.target.matches(".tick-btn i")))
+            markTaskAsDone(e);
+    });
+});
